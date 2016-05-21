2a915f85d50c0024f5f2abbf78fd11421b98095a
Amaury Carrade <amaury@carrade.eu>
2016-05-21 21:27:04 +0200

    1.9 update and sound fix

--- src/main/java/eu/carrade/amaury/LightHeads/HeadsListener.java
+++ src/main/java/eu/carrade/amaury/LightHeads/HeadsListener.java
@@ -1,37 +1,40 @@
 /**
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, you can
+ * obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 package eu.carrade.amaury.LightHeads;
-
-import java.util.Random;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
-public final class HeadsListener implements Listener {
-	
-	private LightHeads p;
-	private Random random;
-	
-	public HeadsListener(LightHeads lightHeads) {
-		p = lightHeads;
-		random = new Random();
-	}
-	
-	/**
-	 * Used to drop the head, following the probability set in the config.
-	 * 
-	 * @param ev
-	 */
-	@EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-	public void onPlayerDeath(PlayerDeathEvent ev) {
-		if(random.nextDouble() <= p.getDropOnDeathProbability() && ev.getEntity().hasPermission(LightHeads.PERM_DEATH_DROP)) {
-			ev.getEntity().getWorld().dropItem(ev.getEntity().getLocation(), p.getHead(ev.getEntity().getName(), 1));
-		}
-	}
+import java.util.Random;
+
+
+public final class HeadsListener implements Listener
+{
+
+    private LightHeads p;
+    private Random random;
+
+    public HeadsListener(LightHeads lightHeads)
+    {
+        p = lightHeads;
+        random = new Random();
+    }
+
+    /**
+     * Used to drop the head, following the probability set in the config.
+     */
+    @EventHandler (ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onPlayerDeath(PlayerDeathEvent ev)
+    {
+        if (random.nextDouble() <= p.getDropOnDeathProbability() && ev.getEntity().hasPermission(LightHeads.PERM_DEATH_DROP))
+        {
+            ev.getEntity().getWorld().dropItem(ev.getEntity().getLocation(), p.getHead(ev.getEntity().getName(), 1));
+        }
+    }
 }
--- src/main/java/eu/carrade/amaury/LightHeads/LightHeads.java
+++ src/main/java/eu/carrade/amaury/LightHeads/LightHeads.java
@@ -1,7 +1,7 @@
 /**
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, you can
+ * obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 package eu.carrade.amaury.LightHeads;
@@ -16,122 +16,148 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class LightHeads extends JavaPlugin {
-	
-	public static final String PERM_GET_SELF   = "heads.self";
-	public static final String PERM_GET_OTHERS = "heads.others";
-	public static final String PERM_GIVE       = "heads.give";
-	public static final String PERM_DEATH_DROP = "heads.deathDrop";
-	
-	double  dropOnDeathProbability;
-	boolean pickupSound;
-	
-	@Override
-	public void onEnable() {
-		this.saveDefaultConfig();
-		
-		pickupSound = getConfig().getBoolean("pickupSound", true);
-		dropOnDeathProbability = Math.min(Math.abs(getConfig().getDouble("dropOnDeathProbability", 0.2d)), 1.0d);
-		
-		if(dropOnDeathProbability > 0) {
-			getServer().getPluginManager().registerEvents(new HeadsListener(this), this);
-		}
-	}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (!cmd.getName().equalsIgnoreCase("head")) return false;
-		
-		String ownerName = null;
-		Player receiver = null;
-		
-		if(args.length == 0) { // /head
-			if(sender instanceof Player) {
-				if(((Player) sender).hasPermission(PERM_GET_SELF)) {
-					ownerName = ((Player) sender).getName();
-					receiver = (Player) sender;
-				}
-				else {
-					sender.sendMessage(ChatColor.RED + getConfig().getString("i18n.disallowed"));
-					return true;
-				}
-			}
-			else {
-				sender.sendMessage(ChatColor.RED + getConfig().getString("i18n.onlyAsAPlayer"));
-				return true;
-			}
-		}
-		else if(args.length == 1) { // /head <headOwner>
-			if(sender instanceof Player) {
-				if(((Player) sender).hasPermission(PERM_GET_OTHERS)) {
-					ownerName = args[0];
-					receiver = (Player) sender;
-				}
-				else {
-					sender.sendMessage(ChatColor.RED + getConfig().getString("i18n.disallowed"));
-					return true;
-				}
-			}
-			else {
-				sender.sendMessage(ChatColor.RED + getConfig().getString("i18n.onlyAsAPlayer"));
-				return true;
-			}
-		}
-		else { // /head <headOwner> <receiver>
-			if(sender.hasPermission(PERM_GIVE)) {
-				ownerName = args[0];
-				receiver = getServer().getPlayer(args[1]);
-				if(receiver == null) {
-					sender.sendMessage(ChatColor.RED + getConfig().getString("i18n.notOnline").replace("{player}", args[1]));
-					return true;
-				}
-			}
-			else {
-				sender.sendMessage(ChatColor.RED + getConfig().getString("i18n.disallowed"));
-				return true;
-			}
-		}
-		
-		ItemStack head = getHead(ownerName, 1);
-		
-		if(receiver.getInventory().addItem(head).size() != 0) {
-			// Inventory was full
-			receiver.getWorld().dropItem(receiver.getLocation(), head);
-			sender.sendMessage(ChatColor.GRAY + getConfig().getString("i18n.inventoryFull"));
-		}
-		else {
-			if(pickupSound) {
-				receiver.playSound(receiver.getLocation(), Sound.ITEM_PICKUP, 0.2f, 1.8f);
-			}
-		}
-		
-		return true;
-	}
-	
-	/**
-	 * Returns an ItemStack representing {@code amount} heads of {@code owner}.
-	 * 
-	 * @param owner The owner.
-	 * @param amount The amount.
-	 * 
-	 * @return The head.
-	 */
-	public ItemStack getHead(String owner, int amount) {
-		ItemStack head = new ItemStack(Material.SKULL_ITEM, amount, (short) 3);
-		
-		SkullMeta sm = (SkullMeta) head.getItemMeta();
-		sm.setOwner(owner);
-		head.setItemMeta(sm);
-		
-		return head;
-	}
-	
-	/**
-	 * Returns the probability a head drop when a player die (between 0.0 and 1.0).
-	 * 
-	 * @return The probability.
-	 */
-	public double getDropOnDeathProbability() {
-		return dropOnDeathProbability;
-	}
+
+public final class LightHeads extends JavaPlugin
+{
+    public static final String PERM_GET_SELF = "heads.self";
+    public static final String PERM_GET_OTHERS = "heads.others";
+    public static final String PERM_GIVE = "heads.give";
+    public static final String PERM_DEATH_DROP = "heads.deathDrop";
+
+    double dropOnDeathProbability;
+    boolean pickupSound;
+
+    @Override
+    public void onEnable()
+    {
+        this.saveDefaultConfig();
+
+        pickupSound = getConfig().getBoolean("pickupSound", true);
+        dropOnDeathProbability = Math.min(Math.abs(getConfig().getDouble("dropOnDeathProbability", 0.2d)), 1.0d);
+
+        if (dropOnDeathProbability > 0)
+        {
+            getServer().getPluginManager().registerEvents(new HeadsListener(this), this);
+        }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+    {
+        if (!cmd.getName().equalsIgnoreCase("head")) return false;
+
+        String ownerName;
+        Player receiver;
+
+        // /head
+        if (args.length == 0)
+        {
+            if (sender instanceof Player)
+            {
+                if (sender.hasPermission(PERM_GET_SELF))
+                {
+                    ownerName = sender.getName();
+                    receiver = (Player) sender;
+                }
+                else
+                {
+                    sender.sendMessage(ChatColor.RED + getConfig().getString("i18n.disallowed"));
+                    return true;
+                }
+            }
+            else
+            {
+                sender.sendMessage(ChatColor.RED + getConfig().getString("i18n.onlyAsAPlayer"));
+                return true;
+            }
+        }
+        // /head <headOwner>
+        else if (args.length == 1)
+        {
+            if (sender instanceof Player)
+            {
+                if (sender.hasPermission(PERM_GET_OTHERS))
+                {
+                    ownerName = args[0];
+                    receiver = (Player) sender;
+                }
+                else
+                {
+                    sender.sendMessage(ChatColor.RED + getConfig().getString("i18n.disallowed"));
+                    return true;
+                }
+            }
+            else
+            {
+                sender.sendMessage(ChatColor.RED + getConfig().getString("i18n.onlyAsAPlayer"));
+                return true;
+            }
+        }
+        // /head <headOwner> <receiver>
+        else
+        {
+            if (sender.hasPermission(PERM_GIVE))
+            {
+                ownerName = args[0];
+                receiver = getServer().getPlayer(args[1]);
+                if (receiver == null)
+                {
+                    sender.sendMessage(ChatColor.RED + getConfig().getString("i18n.notOnline").replace("{player}", args[1]));
+                    return true;
+                }
+            }
+            else
+            {
+                sender.sendMessage(ChatColor.RED + getConfig().getString("i18n.disallowed"));
+                return true;
+            }
+        }
+
+        ItemStack head = getHead(ownerName, 1);
+
+        if (receiver.getInventory().addItem(head).size() != 0)
+        {
+            // Inventory was full
+            receiver.getWorld().dropItem(receiver.getLocation(), head);
+            sender.sendMessage(ChatColor.GRAY + getConfig().getString("i18n.inventoryFull"));
+        }
+        else
+        {
+            if (pickupSound)
+            {
+                receiver.playSound(receiver.getLocation(), Sound.ENTITY_ITEM_PICKUP, 0.2f, 1.8f);
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns an ItemStack representing {@code amount} heads of {@code owner}.
+     *
+     * @param owner The owner.
+     * @param amount The amount.
+     *
+     * @return The head.
+     */
+    public ItemStack getHead(String owner, int amount)
+    {
+        ItemStack head = new ItemStack(Material.SKULL_ITEM, amount, (short) 3);
+
+        SkullMeta sm = (SkullMeta) head.getItemMeta();
+        sm.setOwner(owner);
+        head.setItemMeta(sm);
+
+        return head;
+    }
+
+    /**
+     * Returns the probability a head drop when a player die (between 0.0 and 1.0).
+     *
+     * @return The probability.
+     */
+    public double getDropOnDeathProbability()
+    {
+        return dropOnDeathProbability;
+    }
 }
